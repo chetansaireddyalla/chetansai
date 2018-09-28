db472e72b2985d73d02bff810930bc18edf0d011
chetansaireddyalla <42932904+chetansaireddyalla@users.noreply.github.com>
2018-09-28 12:23:32 +0530

    Add files via upload

---
+++ lbs63.py
@@ -0,0 +1,12 @@
+import random
+
+a={1:'r',2:'p',3:'s'}
+
+while true:
+    p=input("your choice r/p/s: ")
+
+    c=a[random.randint(1,3)]
+
+    print("you choose:",p,"computer chooses:" ,r)
+
+    if    
